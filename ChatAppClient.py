824c479e2c6ef2885790a3dee7f452e9d985d269
xyz-flag <xinyuanzhang@ucsb.edu>
2020-03-11 16:31:46 -0700

    TCP mode 1 work

--- ChatAppClient.py
+++ ChatAppClient.py
@@ -11,7 +11,6 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 from Crypto.Util.Padding import unpad
 from Crypto.Random import get_random_bytes
-import datetime
 
 
 def generate_secret_key():
@@ -20,7 +19,6 @@ def generate_secret_key():
 
 
 def symm_encryption(text, serect_key):
-    #print(datetime.datetime.now().time())
     data = text.encode('ASCII')
     cipher = AES.new(serect_key, AES.MODE_CBC)
     ct_bytes = cipher.encrypt(pad(data, AES.block_size))
@@ -31,18 +29,13 @@ def symm_encryption(text, serect_key):
 
 
 def symm_decryption(ciphertext,serect_key):
-    #print(datetime.datetime.now().time())
-    try:
-        b64 = json.loads(ciphertext)
-        iv = b64decode(b64["iv"])
-        ct = b64decode(b64["ciphertext"])
-        cipher = AES.new(serect_key, AES.MODE_CBC, iv)
-        text = unpad(cipher.decrypt(ct), AES.block_size)
-        data = text.decode('ASCII')
-    except ValueError:
-        print("ValueError")
-    else:
-        return data
+    b64 = json.loads(ciphertext)
+    iv = b64decode(b64["iv"])
+    ct = b64decode(b64["ciphertext"])
+    cipher = AES.new(serect_key, AES.MODE_CBC, iv)
+    text = unpad(cipher.decrypt(ct), AES.block_size)
+    data = text.decode('ASCII')
+    return data
 
 
 def main():
@@ -53,9 +46,8 @@ def main():
     HOST = sys.argv[1]
     PORT = int(sys.argv[2])
     # serect_key = generate_secret_key()
-    serect_key_1 = b'\xdc\x8e@\xa2\x90\x9cF\xf6!\x18\xccK\xc2WWo'
-    serect_key_2 = b'x}\x18\x86\xee+\xf2\x15\xadiBQ>\x80q\x93'
-
+    serect_key = b'\xdc\x8e@\xa2\x90\x9cF\xf6!\x18\xccK\xc2WWo'
+    ivalue = b'\xdc\x8e@\xa2\x90\x9cF\xf6!\x18\xccK\xc2WWo'
 
     MASTER_SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     MASTER_SOCK.settimeout(200)
@@ -73,42 +65,33 @@ def main():
         SOCKET_LIST = [sys.stdin, MASTER_SOCK]
         # Get the list sockets which are readable
         READ_SOCKETS, WRITE_SOCKETS, ERROR_SOCKETS = select.select(SOCKET_LIST, [], [])
-
+        
         for every_sock in READ_SOCKETS: #incoming message from remote server
             if every_sock == MASTER_SOCK: #message is for this client
                 data = every_sock.recv(4096).decode()
-
+                
                 if not data:  #exit
                     print("\nDisconnected from chat server")
                     sys.exit()
-
-                else:
+                
+                else: # print data
+                    # iv_value = data[0]
+                    # ct_value = data[1]
+                    # print(type(iv_value), type(ct_value))
+                    # cipher = json.dumps({'iv': iv_value, 'ciphertext': ct_value})
+                    #print(symm_decryption(data[encryption_header:], serect_key), end = "")
                     if "offline" in data:
                         print(data)
                     else:
-                        print(data)
-                        print(type(data))
-                        #encryption_header = data.index('{')
-                        print (datetime.datetime.now().time())
-                        #print( (data[0:encryption_header]))
-                        #print string about A message from[127.0.0.1:63849]:
-
-                        #print(symm_decryption(data[encryption_header:], serect_key_2), end = "")
-
-                        #如果print这个result，就会出现我截屏里面的第一种的问题
-                        #result = symm_decryption(data[data.index('{'):], serect_key_2)
-
-                        #只print这个可以
-                        print("hhhhh")
-
-
+                        
+                        encryption_header = data.index("{")
+                        print(data[0:encryption_header])
+                        print(symm_decryption(data[encryption_header:], serect_key), end = "")
+            
             else: #user entered a message
                 msg = sys.stdin.readline()
-                print(datetime.datetime.now().time())
-                print("\x1b[1A" + "\x1b[2K", end="") # replace the input value
-
-
-                cipher = symm_encryption(msg,serect_key_1)
+                print("\x1b[1A" + "\x1b[2K", end="") # erase last line
+                cipher = symm_encryption(msg,serect_key)
                 MASTER_SOCK.sendall(cipher.encode())
 
 main()
